a0a6b43a075b02d609c0ab980a32ab3ba9249d57
Nils <nils-bock@t-online.de>
2023-04-21 21:03:52 +0200

    smarty and slim init

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-echo "Hallo Welt";
+
+//require 'smarty/smarty.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Hallo Welt');
+    return $response;
+});
+
+$app->run();
