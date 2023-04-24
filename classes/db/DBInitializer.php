0a1f8c6725f0b4dc414f23e2e8c477b809416ccc
Nils <nils-bock@t-online.de>
2023-04-24 17:20:09 +0200

    updated DBInitializer

--- classes/db/DBInitializer.php
+++ classes/db/DBInitializer.php
@@ -26,13 +26,13 @@ class DBInitializer
         try {
             return $this->db->exec("
             USE `$dbName`;
-            CREATE TABLE `tbl_cash_registers` 
-            (
-                `id_cash_register` int(11) NOT NULL AUTO_INCREMENT,
-                `description` int(11) DEFAULT NULL,
-                PRIMARY KEY (`id_cash_register`)
-            ) 
-            ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+            CREATE TABLE `tbl_cash_registers` (
+              `id_cash_register` int(11) NOT NULL AUTO_INCREMENT,
+              `description` varchar(50) DEFAULT NULL,
+              PRIMARY KEY (`id_cash_register`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
         ");
         } catch (PDOException $e) {
             //echo $e;
@@ -46,19 +46,19 @@ class DBInitializer
         try {
             return $this->db->exec("
             USE `$dbName`;
-            CREATE TABLE `tbl_revenues` (
-              `id_revenue` int(11) NOT NULL AUTO_INCREMENT,
-              `description` varchar(50) DEFAULT NULL,
-              `date` date DEFAULT NULL,
-              `amount` decimal(10,0) DEFAULT NULL,
-              `fk_cash_register` int(11) NOT NULL,
-              `fk_tax_rate` int(11) DEFAULT NULL,
-              PRIMARY KEY (`id_revenue`),
-              KEY `tbl_revenues_tbl_cash_register_id_cash_register_fk` (`fk_cash_register`),
-              KEY `tbl_revenues_tbl_tax_rate_id_tax_rate_fk` (`fk_tax_rate`),
-              CONSTRAINT `tbl_revenues_tbl_cash_register_id_cash_register_fk` FOREIGN KEY (`fk_cash_register`) REFERENCES `tbl_cash_register` (`id_cash_register`),
-              CONSTRAINT `tbl_revenues_tbl_tax_rate_id_tax_rate_fk` FOREIGN KEY (`fk_tax_rate`) REFERENCES `tbl_tax_rate` (`id_tax_rate`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+   CREATE TABLE `tbl_revenues` (
+  `id_revenue` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `fk_cash_register` int(11) NOT NULL,
+  `fk_tax_rate` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_revenue`),
+  KEY `tbl_revenues_tbl_cash_register_id_cash_register_fk` (`fk_cash_register`),
+  KEY `tbl_revenues_tbl_tax_rate_id_tax_rate_fk` (`fk_tax_rate`),
+  CONSTRAINT `tbl_revenues_tbl_cash_register_id_cash_register_fk` FOREIGN KEY (`fk_cash_register`) REFERENCES `tbl_cash_registers` (`id_cash_register`),
+  CONSTRAINT `tbl_revenues_tbl_tax_rate_id_tax_rate_fk` FOREIGN KEY (`fk_tax_rate`) REFERENCES `tbl_tax_rates` (`id_tax_rate`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
 
         ");
@@ -74,14 +74,16 @@ class DBInitializer
             return $this->db->exec("
             USE `$dbName`;
             CREATE TABLE `tbl_cash_status` (
-              `id_cash_status` int(11) NOT NULL,
-              `date` date NOT NULL,
-              `amount` decimal(10,0) NOT NULL,
-              `fk_cash_register` int(11) NOT NULL,
-              PRIMARY KEY (`id_cash_status`),
-              KEY `cash_status_tbl_cash_register_id_cash_register_fk` (`fk_cash_register`),
-              CONSTRAINT `cash_status_tbl_cash_register_id_cash_register_fk` FOREIGN KEY (`fk_cash_register`) REFERENCES `tbl_cash_register` (`id_cash_register`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+  `id_cash_status` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `fk_cash_register` int(11) NOT NULL,
+  PRIMARY KEY (`id_cash_status`),
+  KEY `cash_status_tbl_cash_register_id_cash_register_fk` (`fk_cash_register`),
+  CONSTRAINT `cash_status_tbl_cash_register_id_cash_register_fk` FOREIGN KEY (`fk_cash_register`) REFERENCES `tbl_cash_registers` (`id_cash_register`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
         ");
 
         } catch (PDOException) {
@@ -93,14 +95,12 @@ class DBInitializer
         try {
             return $this->db->exec("
             USE `$dbName`;
-                CREATE TABLE `tbl_tax_rate` (
-              `id_tax_rate` int(11) NOT NULL AUTO_INCREMENT,
-              `tax_rate` decimal(10,0) DEFAULT NULL,
-              `description` varchar(50) DEFAULT NULL,
-              PRIMARY KEY (`id_tax_rate`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
-
-
+             CREATE TABLE `tbl_tax_rates` (
+  `id_tax_rate` int(11) NOT NULL AUTO_INCREMENT,
+  `tax_rate` decimal(10,9) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_tax_rate`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
         ");
 
         } catch (PDOException) {
